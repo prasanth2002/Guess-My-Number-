e3d3883ad960463dd778958799b391f552900fc6
Prasanth <56316048+prasanth2002@users.noreply.github.com>
2024-09-13 12:47:36 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ document.querySelector(".check").addEventListener("click", function () {
     : //if user wins
     guess === number
     ? ((document.querySelector(".message").textContent = "Correct Number"),
-      (document.querySelector(".score").textContent = points ++) ,
+      (document.querySelector(".score").textContent = points++),
       (document.querySelector(".number").textContent = number),
       (document.querySelector("body").style.backgroundColor = "#60b347"),
       (document.querySelector(".highscore").textContent = high++),
@@ -26,16 +26,15 @@ document.querySelector(".check").addEventListener("click", function () {
     : // if it is greater
     life >= 1 && guess > number
     ? ((document.querySelector(".message").textContent = "It is greater "),
-      life--,
-      (document.querySelector(".Life").textContent = life),(
-        (document.querySelector("body").style.backgroundColor = "#222")
-      ))
+      (document.querySelector(".life").textContent = life),
+      (document.querySelector("body").style.backgroundColor = "#222"),
+      life--)
     : // if it is lower
     life >= 1 && guess < number
     ? ((document.querySelector(".message").textContent = "It is lower"),
       (life = life - 1),
-      (document.querySelector(".life").textContent = life),        
-       (document.querySelector("body").style.backgroundColor = "#222"))
+      (document.querySelector(".life").textContent = life),
+      (document.querySelector("body").style.backgroundColor = "#222"))
     : // else
       (document.querySelector(".message").textContent = "GameOver");
 });
@@ -43,7 +42,7 @@ document.querySelector(".check").addEventListener("click", function () {
 document.querySelector(".again").addEventListener("click", function () {
   number = Math.trunc(Math.random() * 20) + 1;
   // hi = Math.max()
-  document.querySelector(".number").textContent = '?';
+  document.querySelector(".number").textContent = "?";
   document.querySelector(".guess").value = "";
   document.querySelector(".message").textContent = "Start Guessing...";
   document.querySelector("body").style.backgroundColor = "#222";
